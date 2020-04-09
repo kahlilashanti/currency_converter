b225467a225bffd835c5b645a7cb4af556f34a10
seetsdev <bushongo@protonmail.com>
2020-04-09 16:42:53 -0700

    added fetch practice

---
+++ script.js
@@ -0,0 +1,11 @@
+// Fetch API practice
+// function calculate() {
+//     fetch('items.json') //returns a promise
+//         .then(res => res.json())
+//         // .then(data => console.log(data));
+//         .then(data => document.body.innerHTML = data[1].id);
+// }
+
+// calculate();
+
+// -------------------------------------------------------------------------------
